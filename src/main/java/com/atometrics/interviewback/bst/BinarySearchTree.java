8f074ca6556176fe9b909b11c02db967c47558b5
Michael Kraxner <michael@atometrics.com>
2024-05-20 22:50:33 +0200

    Adds BST example

---
+++ src/main/java/com/atometrics/interviewback/bst/BinarySearchTree.java
@@ -0,0 +1,30 @@
+package com.atometrics.interviewback.bst;
+
+/**
+ *  A binary search tree (BST) is a binary tree where the value of each node
+ *  - is larger or equal to the values in all the nodes in that node's left subtree and
+ *  - is smaller than the values in all the nodes in that node's right subtree.
+ *
+ *  Write a function that, efficiently with respect to time used, checks if a given binary search tree contains a given value.
+ *
+ *  For example, for the following tree:
+ *
+ *   2
+ *  / \
+ *  1 5
+ *
+ *  n1 (Value: 1, Left: null, Right: null)
+ *  n2 (Value: 2, Left: n1, Right: n3)
+ *  n3 (Value: 5, Left: null, Right: null)
+ *
+ *  Hence, call to contains(n2, 5) should return true since the tree with root at n2 contains number 5.
+ */
+public class BinarySearchTree {
+	public static boolean contains(Node root, int value) {
+
+		// TODO: 1. Write unit tests for this method
+		// TODO: 2. Implement this method
+
+		throw new UnsupportedOperationException("Waiting to be implemented.");
+	}
+}
---
+++ src/main/java/com/atometrics/interviewback/bst/Node.java
@@ -0,0 +1,13 @@
+package com.atometrics.interviewback.bst;
+
+public final class Node {
+	public int value;
+	public Node left;
+	public Node right;
+
+	public Node(int value, Node left, Node right) {
+		this.value = value;
+		this.left  = left;
+		this.right = right;
+	}
+}
