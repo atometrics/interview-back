f58297411b53727c866628c78f4ac03cb4255c86
Michael Kraxner <michael@atometrics.com>
2024-05-21 14:29:54 +0200

    Adds character class

---
+++ src/main/java/com/atometrics/interviewback/characters/Character.java
@@ -0,0 +1,19 @@
+package com.atometrics.interviewback.characters;
+
+public class Character {
+	private Integer id;
+
+	private String name;
+
+	private Integer raceId;
+	private Integer classId;
+	private Integer level;
+	private Integer hitPoints;
+	private Integer strength;
+	private Integer dexterity;
+	private Integer constitution;
+	private Integer intelligence;
+	private Integer wisdom;
+	private Integer charisma;
+	private Integer experience;
+}
